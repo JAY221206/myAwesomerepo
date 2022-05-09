db069f917afc83ebcd8c73fdf0bb65dc5140c667
jay <102516829+JAY221206@users.noreply.github.com>
2022-05-09 09:20:02 +0530

    Update UseReducer.js

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,12 +1,26 @@
 import React, {} from 'react'
+const initialState = 0;
 
 const UseReducer = () => {
+    const reducer = (state, action) => {
+        if(type.action == "INCREMENT")
+        {
+            return state + 1
+        }
+        if(type.action == "DECREMENT")
+        {
+            return state - 1
+        }
+        
+        return state
+        
+    }
 
-    const [state, dispatch] = useReducer(first, second, third)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div>
-        <h1></h1>
+        <h1>{state}</h1>
         <button onClick={() => dispatch({type:"INCREMENT"})}>Inc</button>
         <button onClick={() => dispatch({type:"DECREMENT"})}>Dec</button>
     </div>
